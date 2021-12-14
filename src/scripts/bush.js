7f0827316099a4ac7753ee37e444603ef8cfb3a1
spark <spark11491@gmail.com>
2021-12-14 17:42:14 -0500

    add angellist logo

--- src/scripts/bush.js
+++ src/scripts/bush.js
@@ -1,5 +1,17 @@
 const bushes = document.querySelectorAll(".bush");
 
 export default class Bush {
+    chooseRandomBush() {
+        let lastBush;
 
+        const randomBush = Math.floor(Math.random() * bushes.length);
+        const bush = bushes[randomBush];
+
+        if (bush === lastBush) {
+            return this.chooseRandomBush(bushes);
+        }
+
+        lastBush = bush;
+        return bush;
+    }
 }
